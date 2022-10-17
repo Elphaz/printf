98a85c68dd1dcc12331b6d0fe1463a21a940bc99
Elphaz <elphazshiferaw@gmail.com>
2022-10-17 13:04:22 -0700

    MAIN

--- _printf.c
+++ _printf.c
@@ -3,47 +3,51 @@
 #include <stdarg.h>
 #include <limits.h>
 
-int _printf(const char* format, ...)
+/**
+ * _printf - Write a function that produces output according to a format.
+ * @format: array containing conversion specifiers:
+ * Return:  the number of characters printed.
+ */
+
+int _printf(const char *format, ...)
 {
-    int i, j, strlen = 0;
-    va_list args;
+int i, j, strlen = 0;
+va_list args;
+char *p = NULL;
+char c;
 
-    char* p = NULL;
-    char c;
-    va_start(args, format);
+va_start(args, format);
 
-    for (i = 0; format[i] != '\0'; i++)
-    {
-        strlen++;
-    }
-
-    for (i = 0; i < strlen; i++)
-    {
-        if (format[i] == '%')
-        {
-            if (format[i + 1] == 'c')
-            {
-                c = va_arg(args, int);
-                _putchar(c);
-            }
-            else if (format[i + 1] == 's')
-            {
-                p = va_arg(args, char*);
-                for (j = 0; p[j] != '\0'; j++)
-                {
-                   _putchar(p[j]);
-                }
-            }
-        }
-        else
-        {
-            if (format[i - 1] == '%') continue;
-            _putchar(format[i]);
-        }
-
-    }
-    va_end(args);
-    return (0);
+for (i = 0; format[i] != '\0'; i++)
+{
+strlen++;
 }
 
-
+for (i = 0; i < strlen; i++)
+{
+if (format[i] == '%')
+{
+if (format[i + 1] == 'c')
+{
+c = va_arg(args, int);
+_putchar(c);
+}
+else if (format[i + 1] == 's')
+{
+p = va_arg(args, char *);
+for (j = 0; p[j] != '\0'; j++)
+{
+_putchar(p[j]);
+}
+}
+}
+else
+{
+if (format[i - 1] == '%')
+continue;
+_putchar(format[i]);
+}
+}
+va_end(args);
+return (0);
+}
